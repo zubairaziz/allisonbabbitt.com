9018db3b0029d99e633f2f1bad75645ed4e18c5a
Zubair <zabaziz@trainor.com>
2021-04-11 16:27:56 -0400

    Image slider

--- src/lib/RightPanel.svelte
+++ src/lib/RightPanel.svelte
@@ -1,17 +1,23 @@
+<script>
+  import Slider from './Slider.svelte'
+</script>
+
 <section class="relative w-full h-full p-8">
   <div class="relative w-full h-full bg-lavender-50">
     <div class="w-full h-full flex flex-col justify-between">
-      <div class="spacer h-1/5" />
-      <div>
-        <p class="text-center text-xl md:text-4xl px-4">
+      <div class="h-5/12 w-full">
+        <Slider />
+      </div>
+      <div class="">
+        <p class="text-center text-base md:text-3xl px-4">
           Happy Birthday Allison. <br />
           I hope it's a good one this year 😊<br />
           Looking forward to celebrating it with you!
         </p>
       </div>
-      <div class="flex justify-end items-end">
+      <div class="flex justify-end items-end ">
         <div class="w-1/4 pb-4 md:pb-8">
-          <p class="text-lg md:text-3xl">
+          <p class="text-base md:text-2xl">
             xoxo <br />
             Bear
           </p>
---
+++ src/lib/Slider.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { fade } from 'svelte/transition'
+
+  const carouselPhotos = [
+    './images/image-1.jpg',
+    './images/image-2.jpg',
+    './images/image-3.jpg',
+    './images/image-4.jpg',
+    './images/image-5.jpg',
+    './images/image-6.jpg',
+    './images/image-7.jpg',
+    './images/image-8.jpg',
+    './images/image-9.jpg',
+    './images/image-10.jpg',
+  ]
+
+  let index = 0
+
+  const next = () => {
+    index = (index + 1) % carouselPhotos.length
+  }
+
+  setInterval(() => {
+    next()
+  }, 3500)
+</script>
+
+<div class="relative h-full w-full border-2 border-pink-50">
+  <div class="relative object-cover w-full h-[153.33px] md:h-[286.66px]">
+    {#each [carouselPhotos[index]] as src (index)}
+      <img class="absolute inset-0" transition:fade {src} width="430" height="286.66" alt="" />
+    {/each}
+  </div>
+  <div class="absolute z-10 bg-gradient-to-b from-transparent via-transparent to-pink-50 inset-0" />
+</div>
